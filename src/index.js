6a50845d75c4f9ceea7aeb3d03bc7805a2fdf8ae
Paulo Cerqueira <paulolucas.2010@hotmail.com>
2020-06-22 01:22:41 -0300

    Cors added

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const routes = require('./routes');
+const cors = require('cors')
+
 const port = process.env.PORT || 3333;
 
 const path = require('path');
 
 const app = express();
 
+app.use(cors())
 app.use(express.json());
 app.use(routes);
 
